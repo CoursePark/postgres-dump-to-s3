b4f18dc331eb91d119ee84fad03ab2ce66a65ad5
David Valentiate <me@davidv.ca>
2017-06-12 09:43:36 -0400

    use random temp file to avoid collisions

--- action.sh
+++ action.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 echo "postgres dump to s3 - getting a dump of the database"
-PGOPTIONS="-c statement_timeout=3600000" pg_dump -Fc --no-owner --clean -o $DATABASE_URL > db.dump
+tempfile=$(mktemp -u)
+PGOPTIONS="-c statement_timeout=3600000" pg_dump -Fc --no-owner --clean -o $DATABASE_URL > $tempfile
 echo "postgres dump to s3 - uploading the dump file to s3"
 if [ -n "${DUMP_OBJECT}" ]; then
   object=${DUMP_OBJECT}
@@ -9,5 +10,7 @@ else
   now=$(date +"%Y-%m-%dT%H:%M")
   object=${DUMP_OBJECT_PREFIX}${now}.dump
 fi
-aws --region ${AWS_REGION} s3 cp db.dump s3://${AWS_BUCKET}/$object
+echo postgres dump to s3 - uploading s3://${AWS_BUCKET}/$object
+aws --quiet --region ${AWS_REGION} s3 cp $tempfile s3://${AWS_BUCKET}/$object
+rm $tempfile
 echo "postgres dump to s3 - complete"
