5a8146550a0a06cb155c328e2db299a0dd04cfd2
David Valentiate <me@davidv.ca>
2017-06-11 22:34:17 -0400

    correct documentation and add support for specifying the exact dump object name

--- action.sh
+++ action.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-echo "postgres dump to s3 - getting a dump of database"
+echo "postgres dump to s3 - getting a dump of the database"
 PGOPTIONS="-c statement_timeout=3600000" pg_dump -Fc --no-owner --clean -o $DATABASE_URL > db.dump
-echo "postgres dump to s3 - uploading to s3"
-now=$(date +"%Y-%m-%dT%H:%M")
-object=${DUMP_OBJECT_PREFIX}${now}.dump
-export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
-export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
+echo "postgres dump to s3 - uploading the dump file to s3"
+if [ -n "${DUMP_OBJECT}" ]; then
+  object=${DUMP_OBJECT}
+else
+  now=$(date +"%Y-%m-%dT%H:%M")
+  object=${DUMP_OBJECT_PREFIX}${now}.dump
+fi
 aws --region ${AWS_REGION} s3 cp db.dump s3://${AWS_BUCKET}/$object
 echo "postgres dump to s3 - complete"
