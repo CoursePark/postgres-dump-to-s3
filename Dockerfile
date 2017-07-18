fc7fbf38f88d9301ae61431ccd07bc2c778879c0
David Valentiate <me@davidv.ca>
2017-07-18 11:45:40 -0400

    improvements to build_push process

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,18 @@
 FROM alpine
 
+ARG pg_alpine_branch
 ARG pg_version
-
-# postgresql for pg_dump
-RUN echo 'http://dl-cdn.alpinelinux.org/alpine/v3.4/main' >> /etc/apk/repositories
-
-RUN echo ${pg_version}; apk --no-cache add postgresql-client=${pg_version}
 
 # python for aws-cli, for s3 uploading
 RUN apk --no-cache add python py-pip && \
 	pip install awscli && \
 	apk --purge -v del py-pip
 
-COPY action.sh /
+# postgresql for pg_dump
+RUN echo "http://dl-cdn.alpinelinux.org/alpine/${pg_alpine_branch}/main" >> /etc/apk/repositories
+RUN echo ${pg_version}; apk --no-cache add postgresql-client=${pg_version}
 
+COPY action.sh /
 RUN chmod +x action.sh
 
 CMD echo "${CRON_MINUTE:-$(shuf -i 0-59 -n1)} ${CRON_HOUR:-*} * * * /action.sh" > /var/spool/cron/crontabs/root && crond -d 8 -f
--- build_push.sh
+++ build_push.sh
@@ -1,21 +1,22 @@
 #! /bin/sh
 
 builds=$(echo '
-9.6.3     9.6.3-r0
-9.6       9.6.3-r0
-9         9.6.3-r0
-latest    9.6.3-r0
-9.5.7     9.5.7-r0
-9.5       9.5.7-r0
+9.6.3     9.6.3-r0    v3.6
+9.6       9.6.3-r0    v3.6
+9         9.6.3-r0    v3.6
+latest    9.6.3-r0    v3.6
+9.5.7     9.5.7-r0    v3.4
+9.5       9.5.7-r0    v3.4
 ' | tr -s ' ')
 
 IFS=$'\n'
 for build in $builds; do
   tag=$(echo $build | cut -d ' ' -f 1 )
   pgVersion=$(echo $build | cut -d ' ' -f 2)
+  pgAlpineBranch=$(echo $build | cut -d ' ' -f 3)
   
-  echo docker build --tag bluedrop360/postgres-dump-to-s3:$tag --build-arg pg_version="$pgVersion" .
-  eval docker build --tag bluedrop360/postgres-dump-to-s3:$tag --build-arg pg_version="$pgVersion" .
+  echo docker build --tag bluedrop360/postgres-dump-to-s3:$tag --build-arg pg_version="$pgVersion" --build-arg pg_alpine_branch=$pgAlpineBranch .
+  eval docker build --tag bluedrop360/postgres-dump-to-s3:$tag --build-arg pg_version="$pgVersion" --build-arg pg_alpine_branch=$pgAlpineBranch .
   echo docker push bluedrop360/postgres-dump-to-s3:$tag
   eval docker push bluedrop360/postgres-dump-to-s3:$tag
 done
